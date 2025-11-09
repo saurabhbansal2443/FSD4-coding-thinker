d3eac823bacfd78b398dc0f85093e45bac7d65df
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-09 18:32:34 +0530

    Topic:OS and file system module

--- NODEJS/ES/index.js
+++ NODEJS/ES/index.js
@@ -1,20 +1,32 @@
-import abc, { sum, sub } from "./Maths.js";
-import figlet from "figlet";
-import * as googleTTS from "google-tts-api"; // ES6 or TypeScript
+// import abc, { sum, sub } from "./Maths.js";
+// import figlet from "figlet";
+// import * as googleTTS from "google-tts-api"; // ES6 or TypeScript
 
-// get audio URL
-// const url = googleTTS.getAudioUrl("Hello World I am saurabh", {
-//   lang: "en",
-//   slow: false,
-//   host: "https://translate.google.com",
-// });
-// console.log("url", url); // https://translate.google.com/translate_tts?...
+// // get audio URL
+// // const url = googleTTS.getAudioUrl("Hello World I am saurabh", {
+// //   lang: "en",
+// //   slow: false,
+// //   host: "https://translate.google.com",
+// // });
+// // console.log("url", url); // https://translate.google.com/translate_tts?...
 
-// console.log(abc(2, 3));
+// // console.log(abc(2, 3));
 
-async function doStuff() {
-  const text = await figlet.text("SAURABH");
-  console.log(text);
-}
+// async function doStuff() {
+//   const text = await figlet.text("SAURABH");
+//   console.log(text);
+// }
 
-doStuff();
+// doStuff();
+
+// import os from "os";
+
+// console.log(os.cpus());
+
+import fs from "fs";
+
+// fs.mkdirSync('./tempfolder')
+
+// fs.writeFileSync("./tempfolder/dummy.txt" , " Hey I am dummy text")
+
+console.log(fs.readFileSync("./tempfolder/dummy.txt" , "UTF-8"));
