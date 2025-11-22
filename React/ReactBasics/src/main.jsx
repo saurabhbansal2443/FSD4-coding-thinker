5317379477774ce7960338bcbf59c0f08ed5d682
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-22 12:51:14 +0530

    clean Up project

--- React/ReactBasics/src/main.jsx
+++ React/ReactBasics/src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 
 // let h1 = <h1> I am heading tag </h1>;
 
-function H1() { // component 
-  return <h1> I am h1tag </h1>;
-}
+// function H1() { // component
+//   return <h1> I am h1tag </h1>;
+// }
 
-createRoot(document.getElementById("root")).render(<H1 />);
+createRoot(document.getElementById("root")).render(<App />);
