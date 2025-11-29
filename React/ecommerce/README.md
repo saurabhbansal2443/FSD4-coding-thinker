da0f010aec1a82f8c292588760eec88ab8cc4607
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-29 11:47:23 +0530

    Ecommerce project started and Tailwind setup done

---
+++ React/ecommerce/README.md
@@ -0,0 +1,8 @@
+# React + Vite
+
+// To stop the server press (ctrl + c )
+// to run server npm run dev 
+
+1) React setup using vite 
+2) clean up 
+3) Tailwind install 
---
+++ React/ecommerce/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-sky-900 text-center text-white'>
+      Hello from App 
+    </div>
+  )
+}
+
+export default App
